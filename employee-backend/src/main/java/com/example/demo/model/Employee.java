8b25f3b3ab32cd48cce4c95b01e6e838bd6370bd
Udayakumar1942001 <112074211+Udayakumar1942001@users.noreply.github.com>
2022-12-05 17:15:13 +0530

    first commit

---
+++ employee-backend/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,99 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private String contactNo;
+	private String address;
+	private String salary;
+	
+	public Employee() {
+		
+	}
+	
+	
+
+	public Employee(String firstName, String lastName, String emailId, String contactNo, String address,
+			String salary) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.contactNo = contactNo;
+		this.address = address;
+		this.salary = salary;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+
+}
